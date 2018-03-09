f8528ec2f327202f5839ea9f4c3716d431fbd356
hyamaji <hiroko@hirokoymj.com>
2018-03-09 11:14:15 -0800

    Added new md file

---
+++ test.js
@@ -0,0 +1,14 @@
+const obj = {firstName: 'Hiroko', last: "Yamaji", age: 20};
+
+const {firstName, last} = obj;
+console.log(firstName);
+console.log(last);
+
+
+
+
+// const f = obj.first;
+// const l = obj.last;
+
+// console.log(f);
+// console.log(l);
